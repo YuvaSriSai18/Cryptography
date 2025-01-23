ef29e00bd00fd5711515f8da478a84f126ca35af
Yuva Sri Sai <nanithota18102004@gmail.com>
2025-01-24 00:19:49 +0530

    commit-2

--- Lab_Program_1_2.py
+++ Lab_Program_1_2.py
@@ -11,25 +11,5 @@ def encrypt(plain_text,shift_num):
 
     return result
 
-def decrypt(cipher_text, shift_num):
-    result = ""
-
-    for i in range(len(cipher_text)):
-        char = cipher_text[i]
-
-        if char.isupper():  # For uppercase characters
-            result += chr((ord(char) - shift_num - 65) % 26 + 65)
-        elif char.islower():  # For lowercase characters
-            result += chr((ord(char) - shift_num - 97) % 26 + 97)
-        else:  # Non-alphabetic characters remain unchanged
-            result += char
-
-    return result
-
-
 
 print(encrypt('computerscienceengineeringsrmuniversity' , 4))
-# print(decrypt('gsqtyxivwgmirgiirkmriivmrkwvqyrmzivwmxc' , 4))
-print('\n')
-for i in range(20):
-    print(f'Decrypted Text of Key {i + 1} : {decrypt('PHHW PH DIWHU WKH WRJD SDUWB' , i + 1)}')
